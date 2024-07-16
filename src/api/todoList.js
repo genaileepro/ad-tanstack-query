27a13cec194c2807d4d8497403a60c408d3c2ff6
Daese <daeseonglee7@gmail.com>
2024-07-16 11:07:51 +0900

    sss

--- src/api/todoList.js
+++ src/api/todoList.js
@@ -1,19 +1,19 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const getTodos = async ({ signal }) => {
-  console.log("getTodos 호출");
-  const response = await axios.get("http://localhost:5000/todos", { signal });
-  return response.data;
+    console.log('getTodos 호출');
+    const response = await axios.get('http://localhost:5000/todos', { signal });
+    return response.data;
 };
 
 export const getTodo = async (id) => {
-  console.log("getTodo 호출!");
-  const response = await axios.get(`http://localhost:5000/todos/${id}`);
-  return response.data;
+    console.log('getTodo 호출!');
+    const response = await axios.get(`http://localhost:5000/todos/${id}`);
+    return response.data;
 };
 
 export const addTodo = (newTodo) => {
-  console.log("addTodo 호출");
+    console.log('addTodo 호출');
 
-  return axios.post("http://localhost:5000/todos", newTodo);
+    return axios.post('http://localhost:5000/todos', newTodo);
 };
